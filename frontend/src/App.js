5925389691262b72b278597cae40acf9bee97130
Casta <palolahrn0104@gmail.com>
2020-10-01 15:40:08 -0500

    Se crea proyecto en react, listo para editar

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Conociendo nuestros servicios publicos</h1>
+    </div>
+  );
+}
+
+export default App;
