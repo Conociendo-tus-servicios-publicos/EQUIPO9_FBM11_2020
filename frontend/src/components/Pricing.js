494bd38f0e86c5d771eccd185f6c64b5f7205540
Elianruiz-ux <elyan0811@gmail.com>
2020-10-22 10:37:42 -0500

    Elian modifico pagina

--- frontend/src/components/Pricing.js
+++ frontend/src/components/Pricing.js
@@ -21,11 +21,11 @@ const Pricing = () => {
         <section className="parrafo">
           <h1>¿Qué son los Servicios Públicos?</h1>
         
-          <p> los Servicios públicos son el conjunto de actividades y subsidios permitidos, reservados o exigidos a las administraciones públicas por la legislación en cada Estado, y que tienen como finalidad responder a diferentes imperativos del funcionamiento de la sociedad, y, en última instancia, favorecer la realización efectiva del desarrollo personal, económico, la igualdad y el bienestar social. Suelen ser servicios esenciales, ya que los costos corren a cargo de los contribuyentes a través del Estado (gasto público). Tienen una presencia especialmente significativa en los países de economía mixta que siguen modelos político-económicos como el Estado social o Estado del bienestar. A menudo suele ser tratado en los organismos internacionales como un elemento fundamental dentro del welfare state.1​
-
-Genéricamente, un servicio esencial es una actividad desarrollada por una institución pública o privada con el fin de satisfacer una necesidad básica del conjunto de la sociedad, sin malgastar los recursos públicos.
-
-Los consumidores y usuarios de servicios públicos están protegidos por la Ley de defensa del consumidor, que los ampara frente a las empresas prestadoras de servicios, como el gas, la luz o el teléfono y establece que cuando el consumidor hace un reclamo por fallas en el servicio, las empresas tienen la obligación de registrar su reclamo por cualquier medio disponible.</p>
+          <p>Los servicios públicos son el conjunto de actividades y subsidios permitidos, 
+            reservados o exigidos a las administraciones públicas por la legislación en cada Estado, 
+            y que tienen como finalidad responder a diferentes imperativos del funcionamiento de la 
+            sociedad, y, en última instancia, favorecer la realización efectiva del desarrollo personal,
+             económico, la igualdad y el bienestar social</p>
            </section>
 
            <section className='imagenes'>
--- frontend/src/pages/Informate.js
+++ frontend/src/pages/Informate.js
@@ -15,8 +15,9 @@ function Informate (){
           <h1>Informate</h1>
         </section>
     <section className="parrafo1">
-        <p>Desde el horizonte divisaba mi futuro. Mi espacio lleno de nada y todo la vez. Era como un sueño hecho realidad donde todos eran todos y nadie era ninguno. Justo en ese momento me di cuenta de mi vacío, del espacio que debo llenar para poder sentirme completamente pleno junto a mi yo personal. Lleno de muchas opciones y oportunidades mas que la vida, es un momento de sentirme a mi mismo y el entorno que me rodea.
-          Aquella noche fue épica; llena de incertidumbre, beneficios, dudas, preguntas y respuestas. Fue una noche llena de frutos, llena de pensamientos y de ideas. Aquella noche quedará como un grato recuerdo de lo que fui y lo que comenzaré a ser de aquí en adelante. Es el momento en el que todo hombre llega y debe decidir entre quedarse donde está o comenzar una nueva vida llena de retos e incertidumbre plena pero satisfactoria.</p>
+        <p>Esta es una plataforma que por medio de un comunicado, permite mantener informados a los 
+            ciudadanos de la zona noroccidental de medellin sobre el estado de sus servicios públicos, los usuarios podran informar las 
+            dificultades que presentan los servicios públicos en la zona en que te encuentras.</p>
          <section className="img">
           <img  className="img" src={energiainfo}/>
           </section>
