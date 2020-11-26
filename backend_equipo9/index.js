356e2fb5a575b7318c5cef604b5f48d7e8e4d62f
juan3101 <jdvosorno@gmail.com>
2020-11-26 16:56:33 -0500

    Actualizaciòn

--- backend_equipo9/index.js
+++ backend_equipo9/index.js
@@ -2,8 +2,14 @@ const express =require('express'); //declaracion
 const app = express(); //constructor de la clase express
 const usuarios = require('./routes/usuarios'); //definicion de las rutas
 const fallas = require('./routes/fallas'); 
+const cors = require('cors');
 
-app.set('port',3000)//
+app.set('port',process.env.PORT || 3001)// 
+
+//importante para conexion del front con el back
+//Uso de cors
+app.use(cors({origin: '*'}));
+// conectar con el link de su frontend app.use(cors({origin: 'https://ppi-app.vercel.app/'}))
 
 //middleware
 app.use(express.json());
--- backend_equipo9/routes/usuarios.js
+++ backend_equipo9/routes/usuarios.js
@@ -1,18 +1,81 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const router =express.Router();
 const mysqlConnection = require('../db/db');
 
-router.get('/usuarios',(req,res)=>{
-mysqlConnection.query('SELECT * FROM tbl_usuarios',(err,rows,fiels)=>{
- //si no hay error   
-if(!err){
-//haga  esto
-res.json(rows);
-}else{
-    //  si no (hay error)
- console.log(err);
-}    // fin si 
-})//fin query
-})//fin retorno de todos los estudiantes
 
-module.exports =router;
+router.get('/',(req,res)=>{
+   mysqlConnection.query('SELECT * FROM tbl_usuarios',(err,rows,fiels)=>{
+       if(!err){
+           //haga esto
+           res.json(rows);
+       }else{
+           console.log(err);
+}});//fin si
+})// fin  query
+//retorno de todos los estudiantes
+router.post('/nuevo',(req,res)=>{
+const{id,perfil,correo,contraseña}=req.body;
+
+let registro =[id,perfil,correo,contraseña];
+
+let nuevoUsuario =`INSERT INTO tbl_usuarios(id,perfil,correo,contraseña)
+VALUES(?,?,?,?)`;
+mysqlConnection.query(nuevoUsuario,registro,(err,results,fields)=>{
+if(err){
+   return console.error(err.message());
+}else{
+   res.json({message:` Te has registrado correctamente`})
+}
+})});
+
+//actualizando datos
+router.put('/actualizando/:id',(req,res)=>{
+   const {perfil,correo,contraseña}=req.body;
+   const {id} =req.params;
+   
+
+   mysqlConnection.query(`UPDATE tbl_usuarios SET ,perfil=?,correo=?,contraseña=? WHERE id=?`,
+   [perfil,correo,contraseña,id],(err,rows,fields)=>{
+   if(!err){
+      
+    
+   }else{
+      console.log(err);
+     
+   }
+   })});
+
+ //consultar
+      router.get('/usuario/:id',(req,res)=>{
+      const {id} =req.params;
+      mysqlConnection.query('SELECT * from tbl_usuarios WHERE id= ? ',[id],(err,rows,fields)=>{   
+      if(!err){
+         res.json(rows); 
+      }else{
+      console.log(err);
+      }});
+      })// fin
+       //eliminar un id
+       router.delete('/elimina/:id',(req,res)=>{
+         const {id} =req.params;
+         mysqlConnection.query('DELETE  from tbl_usuarios where documento_id= ? ',[id],(err,rows,fields)=>{   
+         if(!err){
+            res.json({message:`Usuario eliminado`})
+
+         }else{
+         console.log(err);
+         }});
+         })// fin
+
+          //inicio sesion
+                 
+          router.get('/login',(req,res)=>{
+            const {id, contraseña} =req.params;
+            mysqlConnection.query('SELECT * from tbl_usuarios WHERE id= ?  AND contraseña=?',[id, contraseña],(err,rows,fields)=>{
+            if(!err){
+               res.json(rows); 
+            }else{
+            console.log(err);
+            }});  })// fin
+
+module.exports=router;
