be1abd1330473e8f692b56b4aae241e7ece12622
juan3101 <jdvosorno@gmail.com>
2020-11-25 20:57:15 -0500

    cambios

--- frontend/src/components/CardImages3.jsx
+++ frontend/src/components/CardImages3.jsx
@@ -17,21 +17,7 @@ class CardImages3 extends Component {
  <div className="album py-5 bg-light">
     <div className="container">
       <div className="row">
-        <div className="col-md-4">
-          <div className="card mb-4 shadow-sm">
-          <img className="mb-4" src={bloquear_usuarios} alt="" width="150" height="150"/>
-            <div className="card-body">
-              
-              <div className="d-flex justify-content-between align-items-center ">
-                <div className="btn-group">
-                <Link to='/foto'> <button  type="button" className="btn btn-sm btn-outline-primary">Cambiar o editar foto de perfil</button></Link>
-                  
-                </div>
-                
-              </div>
-            </div>
-          </div>
-        </div>
+
         <div className="col-md-4">
           <div className="card mb-4 shadow-sm">
           <img className="mb-4" src={eliminar_mensajes} alt="" width="150" height="150"/>
